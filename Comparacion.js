861e0a5177f4a9f1032542ffae7c06477a160d4c
Alvaro Bazan <alvaarobj@gmail.com>
2025-09-15 13:57:23 -0600

    Funcion pedir numeros

---
+++ Comparacion.js
@@ -0,0 +1,10 @@
+function pedirNumeros(cantidadDenumeros){
+    const numeros=[];
+
+    for (let i = 0; i < indice; i++) {
+       const numeroDado = Number(prompt('Ingresa el numero; ${i+1}'));
+    }
+    return numeros;
+};
+
+function ordenar()
